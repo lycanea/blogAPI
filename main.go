1702fbeb0d25fe4a2f774ac75ec8b573ab450d78
lycanea <lycanea@lycanea.dev>
2025-04-05 13:44:09 +1100

    posts endpoint has like less stuff in it idk its more compact i guess lmao

--- main.go
+++ main.go
@@ -5,16 +5,52 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
+	"github.com/patrickmn/go-cache"
 	"github.com/starshine-sys/pkgo/v2"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+
+type memberCache struct {
+	members *cache.Cache
+}
+
+const (
+	defaultExpiration = 5 * time.Minute
+	purgeTime         = 10 * time.Minute
+)
+
+func newCache() *memberCache {
+	Cache := cache.New(defaultExpiration, purgeTime)
+	return &memberCache{
+		members: Cache,
+	}
+}
+
+func (pkcache *memberCache) read(id string) (item pkgo.Member, ok bool) {
+	member, ok := pkcache.members.Get(id)
+	if ok {
+		log.Println("from cache")
+		res := member.(pkgo.Member)
+		if !ok {
+			return pkgo.Member{}, false
+		}
+		return res, true
+	}
+	return pkgo.Member{}, false
+}
+
+func (pkcache *memberCache) update(id string, member pkgo.Member) {
+	log.Printf("Updating cache for member ID: %s", id)
+	pkcache.members.Set(id, member, cache.DefaultExpiration)
+}
 
 type Post struct {
 	ID      string   `json:"id" bson:"_id"`
@@ -27,11 +63,22 @@ type Post struct {
 	Color   []string `json:"colors"`
 }
 
+type PostResponse struct {
+	ID      string   `json:"id"`
+	Post_ID int32    `json:"post_id" bson:"_post_id_"`
+	Title   string   `json:"title"`
+	Tags    []string `json:"tags"`
+	Author  string   `json:"author"`
+	Color   []string `json:"colors"`
+}
+
 type config struct {
 	SystemID string
 }
 
 func main() {
+	var pkcache = newCache()
+
 	// read config file
 	content, err := os.ReadFile("./config.json")
 	if err != nil {
@@ -93,7 +140,18 @@ func main() {
 			log.Default().Print(posts)
 			return c.Status(500).SendString("Failed to decode posts")
 		}
-		return c.JSON(posts)
+
+		var response []PostResponse
+		for _, post := range posts {
+			response = append(response, PostResponse{
+				ID:     post.ID,
+				Title:  post.Title,
+				Author: post.Author,
+				Color:  post.Color,
+			})
+		}
+
+		return c.JSON(response)
 	})
 
 	app.Get("/posts/:id", func(c *fiber.Ctx) error {
@@ -156,12 +214,14 @@ func main() {
 
 		for _, member := range members {
 			// Assuming each member has fields Name, Description, etc.
-			membersList = append(membersList, map[string]interface{}{
-				"display": member.DisplayName,
-				"name":    member.Name,
-				"id":      member.ID,
-				// Add other fields you want to include
-			})
+			if member.Privacy.Visibility != "private" {
+				membersList = append(membersList, map[string]interface{}{
+					"display": member.DisplayName,
+					"name":    member.Name,
+					"id":      member.ID,
+					// Add other fields you want to include
+				})
+			}
 		}
 
 		return c.JSON(membersList)
@@ -170,13 +230,39 @@ func main() {
 	app.Get("/system/member/:id", func(c *fiber.Ctx) error {
 		idParam := c.Params("id")
 
+		// fmt.Println(pkcache.members.Items())
+		// cached, ok := pkcache.read(idParam)
+		// if ok {
+		// 	if cached.Privacy.Visibility == "private" {
+		// 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+		// 			"error": "Failed to fetch member",
+		// 		})
+		// 	}
+		// 	memberInfo := fiber.Map{
+		// 		"display":     cached.DisplayName,
+		// 		"name":        cached.Name,
+		// 		"description": cached.Description,
+		// 		"id":          cached.ID,
+		// 		"created":     cached.Created,
+		// 		"color":       cached.Color,
+		// 		"avatar":      cached.AvatarURL,
+		// 		"banner":      cached.Banner,
+		// 		"birthday":    cached.Birthday,
+		// 		"pronouns":    cached.Pronouns,
+		// 	}
+		// 	return c.JSON(memberInfo)
+		// }
 		member, err := pk.Member(idParam)
 		if err != nil {
 			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 				"error": "Failed to fetch member",
 			})
 		}
-
+		if member.Privacy.Visibility == "private" {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"error": "Failed to fetch member",
+			})
+		}
 		memberInfo := fiber.Map{
 			"display":     member.DisplayName,
 			"name":        member.Name,
@@ -189,12 +275,12 @@ func main() {
 			"birthday":    member.Birthday,
 			"pronouns":    member.Pronouns,
 		}
-
+		pkcache.update(idParam, member)
 		return c.JSON(memberInfo)
 	})
 
 	app.Get("/modded/message", func(c *fiber.Ctx) error {
-		return c.SendString("test :3")
+		return c.SendString("cuboid now owns/runs the server, i (lycanea) might help with some stuff tho idk meow")
 	})
 
 	log.Fatal(app.Listen(":8080"))
